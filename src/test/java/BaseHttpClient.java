a674200760c1bbf3909bc4d31218793a966457af
Denis Volchenko <volchenko.di@infotek.ru>
2024-02-12 17:04:03 +0300

    -пытаюсь создать курьера

---
+++ src/test/java/BaseHttpClient.java
@@ -0,0 +1,17 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.ErrorLoggingFilter;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+
+public class BaseHttpClient {
+    public static RequestSpecification baseRequestSpec(){
+        return new RequestSpecBuilder()
+                .setBaseUri("https://qa-scooter.praktikum-services.ru")
+                .setRelaxedHTTPSValidation()
+                .addFilter(new RequestLoggingFilter())
+//                .addFilter(new ResponseLoggingFilter())
+//                .addFilter(new ErrorLoggingFilter())
+                .build();
+    }
+}
--- src/test/java/Post.java
+++ src/test/java/Post.java
@@ -4,21 +4,27 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import io.restassured.filter.log.*;
+
+import java.io.File;
 
 public class Post {
     @Before
-    public void setUp() {
-        RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru";
-    }
+//    public void setUp() {
+//        RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru";
+//    }
     @Test
     public void getMyInfoStatusCode() {
-        CreateCourierCard courierCard = new CreateCourierCard(
+        CreateCourierCard courierCard = new CreateCourierCard(//вариант 1
                 "VasyaVasilev",
                 "123",
                 "Vasya");
+        String json = "{\"login\": \"VasyaVasilev\",\"password\": \"123\",\"firstName\": \"Vasya\"}"; //Вариант 2
+        File jsonFile = new File("src/json/newCourier.json");//вариант 3
 
          given()
-                .body(courierCard)
+                 .spec(BaseHttpClient.baseRequestSpec())
+                 .body(json)
                 .when()
                 .post("/api/v1/courier")
                 .then().statusCode(201);
